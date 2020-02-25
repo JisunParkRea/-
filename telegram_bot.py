29daca45a40a0ea365d011ead3fdca7ef964eece
Jisun Park <fpdldk912@gmail.com>
2020-02-25 19:06:07 +0900

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,10 +1,10 @@
 import telegram
 
 # bot 선언, token 넣기
-bot = telegram.Bot(token = '1124663503:AAFrRRYjFpQM1rzG1SwJMDzlXWQr2lTfc7o')
+bot = telegram.Bot(token = 'Here is token')
 
 ## bot에 대한 업데이트 내용 가져오기
 #for i in bot.getUpdates():
 #    print(i.message)
 
-bot.sendMessage(chat_id = '1015736406', text = "테스트입니다.")
+bot.sendMessage(chat_id = 'Here is your id', text = "테스트입니다.")
